c0fa69da2a775099ca28affcee8e5aa9ab900bc6
Antoine PAILLARD <paillard.antoine.ntes@gmail.com>
2014-11-21 17:35:09 +0100

    Commit

---
+++ tests/PasswordGeneratorTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Antoine
+ * Date: 21/11/14
+ * Time: 15:35
+ */
+
+namespace Tests\OKLM\StringGenerator;
+
+class PasswordGeneratorTest extends \PHPUnit_Framework_TestCase
+{
+
+}
